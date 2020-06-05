154c542d6664a73298fe1ec2dfebdce42869340d
yodiaz87 <ydiaztravieso@ncl.com>
2020-06-04 23:53:53 -0400

    -created some endpoints

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+class ApiController extends Controller
+{
+
+    /**
+     * Get github users
+     *
+     * @return [json] user[]
+     */
+    public function githubUsers(Request $request)
+    {
+        $client = new Client();
+        $res = $client->get('https://api.github.com/users');
+
+        return $res->getBody(); //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,22 @@
 <?php
-
 use Illuminate\Http\Request;
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
 });
+
+Route::middleware('auth:api')->get('/githubUsers', 'ApiController@githubUsers');
+
+Route::middleware('auth:api')->get('/notificaions', 'NotificationsController@notifications');
+Route::middleware('auth:api')->post('/notificaions', 'NotificationsController@notifications');
+Route::middleware('auth:api')->put('/notificaions', 'NotificationsController@notifications');
+Route::middleware('auth:api')->delete('/notificaions', 'NotificationsController@notifications');
